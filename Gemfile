71c608930fd31f596a0138cebb029980c4fdd808
Bogdanov Anton <kortirso@gmail.com>
2015-11-16 11:22:27 +0300

    fixes

--- Gemfile
+++ Gemfile
@@ -8,7 +8,6 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'slim'
-gem 'httparty'
 gem 'whenever'
 gem 'rollbar', '~> 2.6.0'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 Gameserver.all.each do |server|
 	server.destroy
 end
@@ -7,4 +9,4 @@ CSV.read("#{Rails.root}/db/gameservers.csv", encoding: 'utf-8', col_sep: ';')[0.
 	Gameserver.create(address: row[0].to_s, ip: row[1].to_s, open: row[2].to_s)
 end
 
-Config.create
+Config.create monster_timeout: 30, monster_percent: 5, monster_heal: 200, streak_bonus: 3, corporate: 150, corporate_periods: 10, corporate_koef: 2, corporate_minimum: 90, corporate_maximum: 210, speed_koef: 2, speed_count: 10, speed_times: 30, triangle_koef: 2, manager_koef: 2, level_percent: 1, level_point: 100, triangle_periods: '', green_mark_points: 3, yellow_mark_points: 2, red_mark_points: -5, default_mark_points: 1, disposals: 10000, dessert_bonus: 5, max_green_time: 90
