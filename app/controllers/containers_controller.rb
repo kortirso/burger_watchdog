3840d1dff3ab96a0e061235abcd1c69df4e4691a
Bogdanov Anton <kortirso@gmail.com>
2015-11-17 18:32:51 +0300

    fix '

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -1,7 +1,15 @@
 class ContainersController < ApplicationController
 	def index
-		str = params[:id]
-		send_file ("/var/www/html/burger_watchdog/#{str}")
+		if params[:id] == 'container.version'
+			send_file("/var/www/html/burger_watchdog/container.version")
+		else
+			File.open("/var/www/html/burger_watchdog/container.version", "r") do |f|
+				f.each_line do |line|
+					version = line
+				end
+			end
+			send_file("/var/www/html/burger_watchdog/burger_king_#{version}.tar")
+		end
 		render nothing: true
 	end
 end
