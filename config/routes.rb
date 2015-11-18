3f95a0f59deb60d579aee48d7f04cbe7c5ff8162
Bogdanov Anton <kortirso@gmail.com>
2015-11-18 10:10:24 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	resources :centrals, only: %i(index update)
-	get 'restore' => 'centrals#restore', as: 'restore_path'
+	get 'restore' => 'centrals#restore', as: 'restore'
 	get 'containers/:id' => 'containers#index'
 	root to: 'status#index'
 end
