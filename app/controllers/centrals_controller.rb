e04d0c2dbb2df9bc0f9ad805da5a31d204bee062
Bogdanov Anton <kortirso@gmail.com>
2015-11-18 10:07:49 +0300

    add restore data button

--- app/controllers/centrals_controller.rb
+++ app/controllers/centrals_controller.rb
@@ -13,6 +13,12 @@ class CentralsController < ApplicationController
 		redirect_to centrals_path
 	end
 
+	def restore
+		Central.first.destroy
+		Central.create
+		render template: 'index'
+	end
+
 	private
 	def central_params
 		params.require(:central).permit :monster_timeout, :monster_percent, :monster_heal, :streak_bonus, :corporate, :corporate_koef, :speed_koef, :triangle_koef, :speed_count, :speed_times, :corporate_minimum, :corporate_maximum, :manager_koef, :level_percent, :level_point, :triangle_periods, :corporate_periods, :disposals, :dessert_bonus, :blinking, :max_green_time, :green_mark_points, :yellow_mark_points, :red_mark_points, :default_mark_points
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	resources :centrals, only: %i(index update)
+	get 'restore' => 'centrals#restore', as: 'restore_path'
 	get 'containers/:id' => 'containers#index'
 	root to: 'status#index'
 end
