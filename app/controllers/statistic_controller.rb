44f306c221c4aef715654e546506ed00d5cb416a
Bogdanov Anton <kortirso@gmail.com>
2015-11-23 16:51:10 +0300

    add times

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -9,7 +9,8 @@ class StatisticController < ApplicationController
 
     def load
         @workdays = Workday.all
-        @workdays = @workdays.set_data(params["params"]["third"]) unless params["params"]["third"].empty?
+        @workdays = @workdays.set_time(params["params"]["first"]) unless params["params"]["first"].empty?
+        @workdays = @workdays.set_data(params["params"]["second"]) unless params["params"]["second"].empty?
         respond_to do |format|
             format.csv { send_data @workdays.build_csv({col_sep: ";", encoding: 'utf-8'})}
         end
--- app/models/workday.rb
+++ app/models/workday.rb
@@ -12,6 +12,49 @@ class Workday < ActiveRecord::Base
         where("restaurant IN (?)", restaurants)
     end
 
+    def self.set_time(x)
+        times = []
+        strings = x.split(".")
+        periods = []
+        strings.each { |string| periods.push(string.split("-")) }
+
+        if periods[1].size == 1
+            if periods[0].size == 1 # 10.10.2015
+                times.push("#{x}")
+            else # 10-15.10.2015
+                (periods[0][0]..periods[0][1]).each do |day|
+                    times.push("#{day}.#{periods[1]}.#{periods[2]}")
+                end
+            end
+        else
+            months = [1,3,5,7,8,10,12]
+            if months.include?(periods[1][0].to_i)
+                finish = 31
+            elsif periods[1][0].to_i == 2
+                finish =28
+            else
+                finish = 29
+            end
+            if periods[0].size == 1 # 10.10-11.2015
+                (periods[0].to_i..finish).each do |day|
+                    times.push("#{day}.#{periods[1][0]}.#{periods[2]}")
+                end
+                (1..periods[0]).each do |day|
+                    times.push("#{day}.#{periods[1][1]}.#{periods[2]}")
+                end
+            else # 10-15.10-11.2015
+                (periods[0][0].to_i..finish).each do |day|
+                    times.push("#{day}.#{periods[1][0]}.#{periods[2]}")
+                end
+                (1..periods[0][1]).each do |day|
+                    times.push("#{day}.#{periods[1][1]}.#{periods[2]}")
+                end
+            end
+        end
+
+        where("day IN (?)", times)
+    end
+
     def self.build_csv(options)
         CSV.generate(options) do |csv|
             csv << column_names
